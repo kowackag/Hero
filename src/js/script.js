e064f02ee4684e3bed4a3bf7fd966fdc059eae17
Kowacka Malgorzata <kowackag@gmail.com>
2022-02-03 23:06:12 +0100

    Finish Task

--- src/js/script.js
+++ src/js/script.js
@@ -2,5 +2,48 @@ import style from './../css/style.css';
 
 import Hero from './Hero';
 
+document.addEventListener('DOMContentLoaded', init);
 
-console.log(Hero)
+const btnPowerUp =  document.querySelector('[name="power-up"]');
+const heroesNum =  document.querySelector('.heroes-number');
+const listContainer = document.querySelector('.list-container');
+
+
+function init () {
+   btnPowerUp.addEventListener('click', powerUp);
+   createHeroes();
+   displayHeroesNum();
+   displayHeroesList();
+}
+
+function displayHeroesNum () {
+    heroesNum.innerText =heroes.length;
+}
+
+function powerUp() {
+    heroes.forEach(item=>item.train());
+    displayHeroesList();
+}
+
+const heroes = [];
+
+function createHeroes() {
+    const hulk =  new Hero("hulk", 10);
+    const geralt = new Hero("geralt", 12);
+    const spiderman = new Hero("spider");
+    heroes.push(hulk);
+    heroes.push(geralt); 
+    heroes.push(spiderman);
+}
+
+
+function displayHeroesList() {
+    listContainer.innerHTML = '';
+    const heroesElement = document.createElement("ul");
+    listContainer.appendChild(heroesElement);  
+    heroes.forEach(({name, power}) => {
+        const heroEl = document.createElement("li");
+        heroEl.innerHTML = `${name}:<span>${power}</span>`;
+        heroesElement.appendChild(heroEl);
+    })
+}
